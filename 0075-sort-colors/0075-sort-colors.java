b08feafc1213df61888eafec17232a97a1992c16
siri2007-vennela <vtu29536@veltech.edu.in>
2026-08-04 15:09:43 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.53%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,26 @@
+   class Solution {
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int mid = 0;
+        int end = nums.length - 1;
+
+        while (mid <= end) {
+            if (nums[mid] == 0) {
+                swap(nums, start, mid);
+                start++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, end);
+                end--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
